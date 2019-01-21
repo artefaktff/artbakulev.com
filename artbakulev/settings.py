e6d96090c70bc785a17b23ab69a006e251f181c5
Artefakt-ff <fasqwerty123@mail.ru>
2019-01-21 22:27:26 +0300

    Add favicon + exit from debug mode

--- artbakulev/settings.py
+++ artbakulev/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+##
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tcs@qjl09ogp&xg&&3_czki&+=eu%6g0oipwvtmymqoj0!317z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.artbakulev.com', 'artbakulev.com', 'localhost', '127.0.0.1']
 
